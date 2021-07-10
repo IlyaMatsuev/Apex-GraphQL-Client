04815b025637d686b466dadb8a6029dad590c0b3
Ilya Matsuev <ilya.matsuev.personal@gmail.com>
2021-07-10 21:39:21 +0200

    First implementation of the gql-builder

---
+++ scripts/sh/init-scratch.sh
@@ -0,0 +1,31 @@
+scratch_alias=$1
+dev_hub_alias=$2
+
+if [[ -z "$scratch_alias" ]]
+then
+	echo "Specify a scratch org alias as the first parameter."
+	exit 1
+fi
+
+if [[ -z "$dev_hub_alias" ]]
+then
+	echo "Specify a dev hub org alias as the second parameter."
+	exit 1
+fi
+
+echo
+echo "Authorizing the dev hub..."
+echo "Please login to your DevHub org."
+sfdx force:auth:web:login -a "$dev_hub_alias"
+
+echo
+echo "Creating scratch..."
+sfdx force:org:create -f ./config/project-scratch-def.json -a "$scratch_alias" -v "$dev_hub_alias"
+
+echo
+echo "Deploying to $scratch_alias..."
+sfdx force:source:push -u "$scratch_alias"
+
+echo
+echo "Deployment has been finished."
+echo "Open the scratch with 'sfdx force:org:open -u $scratch_alias'"
