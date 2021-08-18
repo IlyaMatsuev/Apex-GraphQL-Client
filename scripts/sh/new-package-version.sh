84f506eb8cc037ae51a4d75e368d5abb488bbbbf
Ilya Matsuev <ilya.matsuev.personal@gmail.com>
2021-08-18 23:52:32 +0200

    Create a new package version

--- scripts/sh/new-package-version.sh
+++ scripts/sh/new-package-version.sh
@@ -1,1 +1,1 @@
-sfdx force:package:version:create -p gql-apex-client -d ./src --wait 10 -v gql-client-devhub -x
+sfdx force:package:version:create -p gql-apex-client -d ./src --wait 10 -v gql-client-devhub -x -c
